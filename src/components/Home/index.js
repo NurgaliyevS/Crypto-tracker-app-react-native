614d4f63deb47c942329ff52174aeabc5276f4a5
NurgaliyevS <snurgaliev@cic.kz>
2022-09-04 17:30:33 +0600

    add home component

---
+++ src/components/Home/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+function Home() {
+  return (
+    <View>
+      <Text>HomeComponent</Text>
+    </View>
+  );
+}
+
+export default Home;
