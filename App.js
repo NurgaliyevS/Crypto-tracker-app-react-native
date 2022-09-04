d93cb747ac4233a2a5173e2dbe1a4dc8104c20f8
NurgaliyevS <snurgaliev@cic.kz>
2022-09-04 17:10:59 +0600

    add package for web and styled components

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import styled from 'styled-components/native';
+
+const Title = styled.Text`
+  color: green;
+`;
+
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Title>Title</Title>
+      <StatusBar style='auto' />
     </View>
   );
 }
