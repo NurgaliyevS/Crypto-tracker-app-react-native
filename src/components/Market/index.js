067f2b95a5944da46a55b8985363bc253acb90cb
NurgaliyevS <snurgaliev@cic.kz>
2022-09-04 17:30:44 +0600

    add Market component

---
+++ src/components/Market/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+function Market() {
+  return (
+    <View>
+      <Text>MarketComponent</Text>
+    </View>
+  );
+}
+
+export default Market;
