77692ec2c8f82c473412a47dd0dc74088328ade5
NurgaliyevS <snurgaliev@cic.kz>
2022-09-04 17:30:24 +0600

    Upd app.js

--- App.js
+++ App.js
@@ -1,20 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
 
-import styled from 'styled-components/native';
-
-const Title = styled.Text`
-  color: green;
-`;
+import BottomNavigator from './src/navigation/BottomNavigation';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Title>Title</Title>
-      <StatusBar style='auto' />
-    </View>
-  );
+  return <BottomNavigator />;
 }
 
 const styles = StyleSheet.create({
