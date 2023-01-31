dd452d5919e652d9ee4266cfebfbfa15271b9957
snurgaliev@cic.kz <snurgaliev@cic.kz>
2023-01-31 22:50:44 +0600

    asset component upd

--- src/components/Market/Asset.js
+++ src/components/Market/Asset.js
@@ -1,74 +1,65 @@
-import { View, Text, StyleSheet, Image } from 'react-native';
+import { View, StyleSheet, Image } from "react-native";
 
-import styled from 'styled-components/native';
+import styled from "styled-components/native";
 
 export function Asset({ source, title, price, percentage, isPositive }) {
-    return (
-        <View style={asset.container}>
-            <View style={asset.first}>
-                <View style={crypto.container}>
-                    <Image
-                        source={source}
-                        style={crypto.icon}
-                    />
-                    <TextDefault
-                        style={crypto.title}
-                    >
-                        {title}
-                    </TextDefault>
-                </View>
-
-            </View>
-            <View style={asset.second}>
-                <TextPrice style={asset.title}>{price}</TextPrice>
-            </View>
+  return (
+    <View style={asset.container}>
+      <View style={asset.first}>
+        <View style={crypto.container}>
+          <Image source={source} style={crypto.icon} />
+          <TextDefault style={crypto.title}>{title}</TextDefault>
         </View>
-    )
+      </View>
+      <View style={asset.second}>
+        <TextPrice style={[asset.title, crypto.title]}>{price}</TextPrice>
+        <TextPrice style={[asset.percentageText, isPositive === true ? asset.percentageUp : asset.percentageDown]}>{percentage}</TextPrice>
+      </View>
+    </View>
+  );
 }
 
-const style = StyleSheet.create({
-    TextDefaultCommon: {
-        color: '#FFFFFF',
-    },
-});
-
 const asset = StyleSheet.create({
-    container: {
-        flex: 1,
-        flexDirection: 'row',
-        paddingTop: 25,
-        paddingLeft: 30,
-        paddingRight: 30,
-    },
-    title: {
-        fontSize: 15
-    },
-    first: {
-        flex: 1.5,
-        backgroundColor: 'white',
-    },
-    second: {
-        flex: 1.5,
-        backgroundColor: 'red',
-        flexDirection: 'column'
-    },
+  title: {
+    fontSize: 15,
+  },
+  first: {
+    flex: 1,
+  },
+  second: {
+    flex: 1,
+  },
+  container: {
+    flex: 1,
+    flexDirection: 'row'
+  },
+  percentageDown: {
+    color: 'red'
+  },
+  percentageUp: {
+    color: 'green'
+  },
+  percentageText: {
+    fontSize: 13
+  }
 });
 
 const crypto = StyleSheet.create({
-    container: {
-        flexDirection: 'row'
-    },
-    icon: {
-        width: 30,
-        height: 30,
-        marginTop: 20,
-        width: '22%',
-        backgroundColor: 'black',
-    },
-    price: {
-        marginTop: 34,
-    }
-})
+  container: {
+    flexDirection: "row",
+  },
+  icon: {
+    height: 40,
+    marginTop: 20,
+    width: '25%',
+    backgroundColor: 'black',
+  },
+  title: {
+    paddingLeft: 10,
+    paddingTop: 30,
+    fontSize: 14,
+  },
+});
 
 const TextPrice = styled.Text`
   color: white;
