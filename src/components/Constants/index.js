a12c5f0a84053abc244a17968a27d5066839e70f
snurgaliev@cic.kz <snurgaliev@cic.kz>
2023-01-31 22:49:44 +0600

    add constants

---
+++ src/components/Constants/index.js
@@ -0,0 +1,4 @@
+export const bitcoinImage = require('../../../assets/bitcoin.jpg');
+export const ethereumImage = require('../../../assets/ethereum.jpg');
+export const polygonImage = require('../../../assets/polygon.jpg');
+export const tetherImage = require('../../../assets/tether.jpg');
