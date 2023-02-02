1d25b9029b56cb449064a7567fd88e30d895b3fc
snurgaliev@cic.kz <snurgaliev@cic.kz>
2023-02-02 21:39:52 +0600

    add statusbar

--- App.js
+++ App.js
@@ -1,16 +1,21 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, StatusBar } from "react-native";
 
-import BottomNavigator from './src/navigation/BottomNavigation';
+import BottomNavigator from "./src/navigation/BottomNavigation";
 
 export default function App() {
-  return <BottomNavigator />;
+  return (
+    <>
+      <StatusBar backgroundColor="aqua" barStyle="light-content" />
+      <BottomNavigator />
+    </>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
