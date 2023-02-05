a0f284cdf14e78a7ac95396cd846da3548e2ff2c
snurgaliev@cic.kz <snurgaliev@cic.kz>
2023-02-05 20:30:17 +0600

    remove error

--- src/store.js
+++ src/store.js
@@ -3,7 +3,11 @@ import { configureStore } from "@reduxjs/toolkit";
 import homeSlice from "./components/Slices/homeSlice";
 
 export const store = configureStore({
-    reducer: {
-        home: homeSlice
-    }
-})
+  reducer: {
+    home: homeSlice,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
+});
