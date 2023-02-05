3de04af84197c5e96dfb099e55fb80d5f6886326
snurgaliev@cic.kz <snurgaliev@cic.kz>
2023-02-05 20:31:03 +0600

    upd record

--- src/components/Home/Record.js
+++ src/components/Home/Record.js
@@ -1,23 +1,54 @@
 import React from "react";
 
-import { View, StyleSheet, Image } from "react-native";
+import { View, StyleSheet, Image, Pressable } from "react-native";
+import { useDispatch, useSelector } from "react-redux";
 
 import styled from "styled-components/native";
+
+import { setRecord, setIsOpenModal } from "../Slices/homeSlice";
 
 const TextDefault = styled.Text`
   color: white;
 `;
 
-export function Record({ source, title, desiredPrice, currentPrice }) {
+export function Record({ source, title, desiredPrice, currentPrice, id }) {
+  const dispatch = useDispatch();
+  const { records } = useSelector((state) => state.home);
+
+  const setRecordMethod = (id) => {
+    findRecord(id);
+  };
+
+  const findRecord = (id) => {
+    if (Object.keys(records).length) {
+      const allValues = Object.values(records);
+      allValues.map((element) => {
+        if (element.id === id) {
+          dispatch(setRecord(element));
+          dispatch(setIsOpenModal(true));
+        }
+      });
+    }
+  };
+
   return (
-    <View style={[style.container]}>
+    <Pressable
+      onPress={() => {
+        setRecordMethod(id);
+      }}
+      style={style.container}
+    >
       <View style={crypto.container}>
         <Image source={source} style={crypto.image} />
       </View>
       <TextDefault style={[crypto.text, crypto.title]}>{title}</TextDefault>
-      <TextDefault style={[crypto.text, crypto.price]}>{desiredPrice}</TextDefault>
-      <TextDefault style={[crypto.text, crypto.price]}>{currentPrice}</TextDefault>
-    </View>
+      <TextDefault style={[crypto.text, crypto.price]}>
+        {desiredPrice}
+      </TextDefault>
+      <TextDefault style={[crypto.text, crypto.price]}>
+        {currentPrice}
+      </TextDefault>
+    </Pressable>
   );
 }
 
@@ -45,13 +76,13 @@ const crypto = StyleSheet.create({
   text: {
     paddingTop: 15,
     flex: 1,
-    flexWrap: 'wrap'
+    flexWrap: "wrap",
   },
   title: {
     paddingLeft: 15,
-    textAlign: 'left'
-  },   
+    textAlign: "left",
+  },
   price: {
-    textAlign: 'right',
-  }
+    textAlign: "right",
+  },
 });
